134cdfe0e772238dfd12253097c518b437dc0f5a
Sven Sutter <sven.sutter@sotax.com>
2024-07-02 11:10:29 +0200

    Add Navigation/Fonts/Styles

---
+++ FarmingSimApp/Shared/View/Controls/NavigationButton.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace FarmingSimApp.Shared.View.Controls;
+
+public class NavigationButton : RadioButton
+{
+    public static readonly StyledProperty<IBrush> HoverBrushProperty =
+        AvaloniaProperty.Register<NavigationButton, IBrush>(
+            nameof(HoverBrush), new SolidColorBrush(Colors.Transparent));
+
+    public IBrush HoverBrush
+    {
+        get => GetValue(HoverBrushProperty);
+        set => SetValue(HoverBrushProperty, value);
+    }
+}
